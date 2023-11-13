aef17b476a9e490de9f90ece900b69c962b158a3
Azer Lahmar <azer.lahmar@esprit.tn>
2023-11-13 02:32:30 +0100

    fix errors for our project

--- app/src/main/java/com/esprit/validationlastwoop/VariousActivity.java
+++ app/src/main/java/com/esprit/validationlastwoop/VariousActivity.java
@@ -1,23 +1,15 @@
 package com.esprit.validationlastwoop;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.GridLayout;
-
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-
-public class SearchActivity extends AppCompatActivity {
+public class VariousActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_interface);
+        setContentView(R.layout.various_interface);
 
     }
-
 }
